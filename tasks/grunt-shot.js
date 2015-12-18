178b55f022d198247829c65f3b0ef7967670b2d5
David Bielik <dbielik90@gmail.com>
2015-12-18 14:19:58 -0500

    starting

--- tasks/grunt-shot.js
+++ tasks/grunt-shot.js
@@ -5,46 +5,99 @@
  * Copyright (c) 2015 David Bielik
  * Licensed under the MIT license.
  */
+(function() {
+    /* global module ,require, __dirname */
+    'use strict';
+    var P = require('bluebird');
+    var phantom = require('phantom');
+    module.exports = function(grunt) {
 
-'use strict';
+        // Please see the Grunt documentation for more information regarding task
+        // creation: http://gruntjs.com/creating-tasks
 
-module.exports = function(grunt) {
+        grunt.registerMultiTask('shot', 'Automate screenshots for specified pages.', function() {
+            // var options = this.options({
+            //     punctuation: '.',
+            //     separator: ', '
+            // });
+            // Iterate over all specified file groups.
+            var done = this.async();
+            var options = this.data.options || {};
+            var url = (options.protocol || 'http') + '://' + (options.host || 'localhost');
+            var files = this.files[0].orig.src;
+            var promises = [];
+            for (var i = 0, l = files.length; i < l; i++) {
+                var file = files[i];
+                if (!/^\//.test(file)) {
+                    grunt.log.warn('file paths should be relative urls (ex: /login). You provided: "' + file + '"');
+                    continue;
+                }
+                console.log('pushing ' + url + file);
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('shot', 'Automate screenshots for specified pages.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+                promises.push(screenshot(url + file, options.output));
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+                // Concat specified files.
+                // var src = f.src.filter(function(filepath) {
+                //     // Warn on and remove invalid source files (if nonull was set).
+                //     if (!grunt.file.exists(filepath)) {
+                //         grunt.log.warn('Source file "' + filepath + '" not found.');
+                //         return false;
+                //     } else {
+                //         return true;
+                //     }
+                // }).map(function(filepath) {
+                //     // 
+                //     return filepath;
+                // }).join(grunt.util.normalizelf(options.separator));
+
+                // // Handle options.
+                // src += options.punctuation;
+
+                // // Write the destination file.
+                // console.log(src);
+
+                // Print a success message.
+                //dgrunt.log.writeln('File "' + f.dest + '" created.');
+            }
+            P.all(promises).then(onSuccess).catch(onError).done(done);
+        });
+
+        function screenshot(url, output) {
+            var defer = P.defer();
+            var timeout = 10000;
+            output = output || __dirname + '/screenshots/';
+
+            phantom.create({
+                parameters: {
+                    'load-images': 'yes',
+                    'ignore-ssl-errors': 'yes'
+                }
+            }, function(ph) {
+                ph.createPage(function(page) {
+                    page.set('settings.loadImages', true);
+                    page.open(url, function(status) {
+                        console.log('opened ' + url + ': ', status);
+                        setTimeout(function() {
+                            var file = output + encodeURIComponent(url.replace(/^https?:\/\//,'')) + '.png';
+                            page.render(file, function(status){
+                                console.log('saved to ' + file, status);
+                                defer.resolve(url);
+                            });
+                        }, timeout);
+                    });
+                });
+            });
+            return defer.promise;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        function onSuccess(data) {
+            console.log('# On success: ', data);
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        function onError(error) {
+            console.log('# On error: ', error);
+        }
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
-};
+    };
+})();
